1e13303ec04a148db1ac79bd073247e62685cc1f
Kevin Diska Reynaldi <kevindiska@gmail.com>
2018-12-28 02:02:59 +0700

    Making it usable :)

--- admin/admin/galeri/list.php
+++ admin/admin/galeri/list.php
@@ -9,24 +9,26 @@
 			<thead>
 				<tr>
 					<th width="1%">No</td>
-					<th width="10%">Nama</td>
-					<th width="10%">Foto</td>
-					<th width="10%">Keterangan</td>
+					<th width="10%">Id Galeri</td>
+					<th width="10%">Judul Galeri</td>
+					<th width="10%">Gambar Galeri</td>
+					<th width="10%">Album Id</td>
 					<th width="20%">Option</td>
 				</tr>
 			</thead>
 			<tbody>
 			<?php 
 				$no = 1;
-				$sql=$db->prepare("SELECT * FROM galeri ORDER BY nama ASC");
+				$sql=$db->prepare("SELECT * FROM galeri ORDER BY id_galeri ASC");
 				$sql->execute();
 				while($hasil=$sql->FETCH(PDO::FETCH_ASSOC)){
 			?>
 			<tr>
 				<td><?php echo $no; ?></td>
-				<td><?php echo $hasil['nama'];?></td>
-				<td><img src="../data/<?php echo $hasil['foto'];?>" /></td>
-				<td><?php echo $hasil['keterangan'];?></td>
+				<td><?php echo $hasil['id_galeri'];?></td>
+				<td><?php echo $hasil['judul_galeri'];?></td>
+				<td><img src="../data/<?php echo $hasil['gambar_galeri'];?>" /></td>
+				<td><?php echo $hasil['album_id'];?></td>
 				<td>
 					<button class="btn btn-warning" onclick="javascript:window.location.href='index.php?module=galeri&action=add&id=<?php echo $hasil['id_galeri'];?>'">EDIT</button>
 					<button class="btn btn-danger" onclick="javascript:window.location.href='index.php?module=galeri&action=proses&id=<?php echo $hasil['id_galeri'];?>&proc=delete'">DELETE</button>
