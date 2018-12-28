e6f41af4279ef710c605f6428468a3d8de2b0c8c
Kevin Diska Reynaldi <kevindiska@gmail.com>
2018-12-28 13:52:32 +0700

    Make listing possible

--- admin/admin/kategori_paket/list.php
+++ admin/admin/kategori_paket/list.php
@@ -2,34 +2,32 @@
 	if(@$_SESSION['username'] != ''){
 ?>
 <div class="fix">
-	<h1>Galeri</h1>
-	<button class="btn btn-primary" onclick="javascript:window.location.href='index.php?module=galeri&action=add'">ADD</button>
+	<h1>Kategori Paket</h1>
+	<button class="btn btn-primary" onclick="javascript:window.location.href='index.php?module=kategori_paket&action=add'">ADD</button>
 	<div class="fix-table">
 		<table id="example1" class="table table-bordered table-striped">
 			<thead>
 				<tr>
 					<th width="1%">No</td>
-					<th width="10%">Nama</td>
-					<th width="10%">Foto</td>
-					<th width="10%">Keterangan</td>
+					<th width="10%">Id Kategori</td>
+					<th width="10%">Kategori</td>
 					<th width="20%">Option</td>
 				</tr>
 			</thead>
 			<tbody>
 			<?php 
 				$no = 1;
-				$sql=$db->prepare("SELECT * FROM galeri ORDER BY nama ASC");
+				$sql=$db->prepare("SELECT * FROM kategori_paket ORDER BY id_kategori ASC");
 				$sql->execute();
 				while($hasil=$sql->FETCH(PDO::FETCH_ASSOC)){
 			?>
 			<tr>
 				<td><?php echo $no; ?></td>
-				<td><?php echo $hasil['nama'];?></td>
-				<td><img src="../data/<?php echo $hasil['foto'];?>" /></td>
-				<td><?php echo $hasil['keterangan'];?></td>
+				<td><?php echo $hasil['id_kategori'];?></td>
+				<td><?php echo $hasil['kategori'];?></td>
 				<td>
-					<button class="btn btn-warning" onclick="javascript:window.location.href='index.php?module=galeri&action=add&id=<?php echo $hasil['id_galeri'];?>'">EDIT</button>
-					<button class="btn btn-danger" onclick="javascript:window.location.href='index.php?module=galeri&action=proses&id=<?php echo $hasil['id_galeri'];?>&proc=delete'">DELETE</button>
+					<button class="btn btn-warning" onclick="javascript:window.location.href='index.php?module=kategori_paket&action=add&id=<?php echo $hasil['id_kategori'];?>'">EDIT</button>
+					<button class="btn btn-danger" onclick="javascript:window.location.href='index.php?module=kategori_paket&action=proses&id=<?php echo $hasil['id_kategori'];?>&proc=delete'">DELETE</button>
 				</td>
 			</tr>
 			<?php
